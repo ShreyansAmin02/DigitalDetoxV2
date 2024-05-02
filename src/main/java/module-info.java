f1cf66e2891ce854520818b84978a180ebca20ae
ShreyansAmin02 <n11385781@qut.edu.au>
2024-05-02 10:02:51 +1000

    test file keeps throwing an error

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module com.example.digitaldetox {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+    requires java.desktop;
+    requires org.junit.jupiter.api;
+
+
+    opens com.example.digitaldetox to javafx.fxml;
+    exports com.example.digitaldetox;
+    exports com.example.digitaldetox.controller;
+    opens com.example.digitaldetox.controller to javafx.fxml;
+}
