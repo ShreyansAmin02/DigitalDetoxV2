efbc07fca6ede8ba001981010e1a30eca2e2d933
Alex Scott <Alexscott3512@gmail.com>
2024-05-16 15:41:07 +1000

    blank Files

---
+++ src/main/java/com/example/digitaldetox/FeatureTimer/CountdownTimer.java
@@ -0,0 +1,16 @@
+package com.example.digitaldetox.FeatureTimer;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class CountdownTimer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
